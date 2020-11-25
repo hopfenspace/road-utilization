6be9fb25986d22e287b8a180b93c12ce980a915d
myOmikron <accounts@omikron.dev>
2020-11-25 23:08:08 +0100

    Added models

--- api/road_utilization/models.py
+++ api/road_utilization/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+from django.db.models import Model, CharField, IntegerField, FloatField, DateTimeField, ForeignKey, CASCADE, \
+    ManyToManyField
 
-# Create your models here.
+
+class Device(Model):
+    device_id = CharField(default="", max_length=255)
+
+
+class SensorPosition(Model):
+    device = ForeignKey(Device, on_delete=CASCADE)
+    geo_uri = CharField(default="", max_length=255)
+
+
+class RawData(Model):
+    device = ForeignKey(Device, on_delete=CASCADE)
+    count_car = IntegerField(default=0)
+    count_truck = IntegerField(default=0)
+    battery = FloatField(default=0)
+    timestamp = DateTimeField()
+    rssi = IntegerField(default=0)
+
+
+class Coordinate(Model):
+    lat = FloatField(default=0)
+    lon = FloatField(default=0)
+
+
+class RoadType(Model):
+    highway_type = CharField(default="", max_length=255)
+
+
+class RoadStretch(Model):
+    osm_id = CharField(default="", max_length=255)
+    road_type = ForeignKey(RoadType, on_delete=CASCADE)
+    coordinates = ManyToManyField(Coordinate)
+
+
+class Road(Model):
+    name = CharField(default="", max_length=255)
+    road_stretches = ManyToManyField(RoadStretch)
+
+    def __str__(self):
+        return self.name
