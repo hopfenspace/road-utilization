a886051a8d26ba3a17d8c37b6400bc1eb3c2c28c
myOmikron <accounts@omikron.dev>
2020-11-25 21:57:45 +0100

    Added models for devices, sensor_positions, coordinates, road_types and roads

--- api/api/models.py
+++ api/api/models.py
@@ -1,10 +1,36 @@
-from django.db.models import Model, CharField, IntegerField, FloatField, DateTimeField
+from django.db.models import Model, CharField, IntegerField, FloatField, DateTimeField, ForeignKey, CASCADE, \
+    ManyToManyField
+
+
+class Device(Model):
+    device_id = CharField(default="", max_length=255)
+
+
+class SensorPosition(Model):
+    device = ForeignKey(Device, on_delete=CASCADE)
+    geo_uri = CharField(default="", max_length=255)
 
 
 class RawData(Model):
-    device_id = CharField(default="", max_length=255)
+    device = ForeignKey(Device, on_delete=CASCADE)
     count_car = IntegerField(default=0)
     count_truck = IntegerField(default=0)
     battery = FloatField(default=0)
     timestamp = DateTimeField()
     rssi = IntegerField(default=0)
+
+
+class Coordinate(Model):
+    lat = FloatField(default=0)
+    lon = FloatField(default=0)
+
+
+class RoadType(Model):
+    highway_type = CharField(default="", max_length=255)
+
+
+class Road(Model):
+    name = CharField(default="", max_length=255)
+    osm_id = CharField(default="", max_length=255)
+    road_type = ForeignKey(RoadType, on_delete=CASCADE)
+    coordinates = ManyToManyField(Coordinate)
