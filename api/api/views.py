4dd6ca5bb3e1b8b75ee8b2b1b47d9c50497b82e6
myOmikron <accounts@omikron.dev>
2020-11-25 02:02:53 +0100

    Fixed empty request tm

--- api/api/views.py
+++ api/api/views.py
@@ -1,13 +1,11 @@
+import json
+
 from django.http import HttpResponse
 from django.views.generic.base import View
 
 
 class PutView(View):
 
-    def get(self, request, *args, **kwargs):
-        print(request.GET)
-        return HttpResponse("", status=200)
-
     def post(self, request):
-        print(request.POST)
+        print(json.loads(request.body))
         return HttpResponse("OK", status=200)
