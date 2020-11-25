8d162f07fec755816573a26b1e8573fb2acee499
myOmikron <accounts@omikron.dev>
2020-11-25 22:23:14 +0100

    Added app for api calls

---
+++ api/road_utilization/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from api.models import *
+
+admin.site.register(Device)
+admin.site.register(SensorPosition)
+admin.site.register(RawData)
+admin.site.register(RoadType)
+admin.site.register(Coordinate)
+admin.site.register(Road)
+from django.contrib import admin
+
+# Register your models here.
---
+++ api/road_utilization/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RoadUtilizationConfig(AppConfig):
+    name = 'road_utilization'
