e79d57cb8edc736746893134bb73546fbea2cd06
myOmikron <accounts@omikron.dev>
2020-11-25 23:08:32 +0100

    Added models to admin site

--- api/road_utilization/admin.py
+++ api/road_utilization/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 
-from api.models import *
+from road_utilization.models import *
 
 admin.site.register(Device)
 admin.site.register(SensorPosition)
 admin.site.register(RawData)
 admin.site.register(RoadType)
 admin.site.register(Coordinate)
+admin.site.register(RoadStretch)
 admin.site.register(Road)
-from django.contrib import admin
-
-# Register your models here.
