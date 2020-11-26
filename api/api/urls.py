a5a0f3579bf895c13cdec245c1eacd701e9a652a
myOmikron <accounts@omikron.dev>
2020-11-26 02:27:51 +0100

    Added api call getRoadUtilization

--- api/api/urls.py
+++ api/api/urls.py
@@ -6,5 +6,6 @@ from road_utilization import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/put', views.PutView.as_view()),
-    path('api/import', views.ImportRoads.as_view())
+    path('api/import', views.ImportRoads.as_view()),
+    path('api/getRoadUtilization', views.RoadUtilization.as_view())
 ]
--- api/road_utilization/views.py
+++ api/road_utilization/views.py
@@ -1,11 +1,11 @@
 import json
 from datetime import datetime
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic.base import View
 
 from api import scripts
-from road_utilization.models import RawData, Device
+from road_utilization.models import RawData, Device, RoadStretch
 
 
 class ImportRoads(View):
@@ -15,7 +15,6 @@ class ImportRoads(View):
 
 
 class PutView(View):
-
     def post(self, request):
         data = json.loads(request.body)
         device, created = Device.objects.get_or_create(device_id=data["dev_id"])
@@ -36,3 +35,28 @@ class PutView(View):
         raw_data.save()
 
         return HttpResponse("OK", status=200)
+
+
+class RoadUtilization(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "success": True,
+            "result": {}
+        }
+        if "road_stretch" not in request.GET:
+            return JsonResponse({"success": False, "result": "Please specify the parameter road_stretch"}, status=400)
+        osm_id = request.GET["road_stretch"]
+        try:
+            road_stretch_object = RoadStretch.objects.get(osm_id=osm_id)
+            data["result"]["osm_id"] = road_stretch_object.osm_id
+            data["result"]["coordinates"] = []
+            for coordinate in road_stretch_object.coordinates.all():
+                data["result"]["coordinates"].append({
+                    "lat": coordinate.lat,
+                    "long": coordinate.lon
+                })
+            data["result"]["road_type"] = road_stretch_object.road_type.highway_type
+        except RoadStretch.DoesNotExist:
+            return JsonResponse({"success": False, "result": f"RoadStretch was not found with osm_id {osm_id}"},
+                                status=400)
+        return JsonResponse(data, safe=False)
