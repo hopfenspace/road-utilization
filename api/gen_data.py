c00aed607c963d2a15da7a76f0e64acd67833d36
Chris <chris@hopfen.space>
2020-11-28 04:34:01 +0100

    Added the first version of the script to generate random data

---
+++ api/gen_data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import random
+import datetime
+import urllib.parse
+
+
+# Static data
+base_url = "https://lora.omikron.dev/api/"
+device_id = "random_data_generator"
+
+
+def generate_vehicle_data() -> dict:
+    """Generate random vehicle data"""
+
+    return {str(k): random.randint(0, 5) for k in range(1, random.randint(0, 10)}
+
+
+def generate_payload() -> dict:
+    """Generate random payload data for /put"""
+
+    return {
+        "dev_id": device_id,
+        "payload_fields": {
+            "battery": random.random() * 100,
+            "vehicles": generate_vehicle_data()
+        },
+        "metadata": {
+            "time": datetime.datetime.now().isoformat(),
+            "gateways": [
+                {
+                    "rssi": -random.randint(0, 100)
+                }
+            ]
+        }
+    }
+
