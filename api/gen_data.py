6351be470f110eebc0b89733c7535aec1ef967e8
Chris <chris@hopfen.space>
2020-11-28 16:32:18 +0100

    Added the extended version of the RandomTrafficGenerator

--- api/gen_data.py
+++ api/gen_data.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 import random
 import datetime
+
+import requests
 
 
 def generate_vehicle_data() -> dict:
@@ -37,10 +40,73 @@ def generate_payload(device_id: str) -> dict:
 class RandomTrafficGenerator:
     """Generator for random traffic data"""
 
-    def __init__(self, device_id: str):
+    allowed_roads = ["primary", "secondary", "tertiary"]
+    base_url = "https://lora.omikron.dev/api/"
+    cached_roads = None
+    cached_road_keys = None
+
+    def __init__(self, device_id: str, coordinate: dict, road_stretch: str):
         self.device_id = device_id
-        self.base_url = "https://lora.omikron.dev/api/"
-        self.allowed_roads = ["primary", "secondary", "tertiary"]
+        self.coordinate = coordinate
+        self.road_stretch = road_stretch
+
+        self.set_position()
+
+    def set_position(self):
+        """Tell the sensor's position to the backend"""
+
+        r = requests.post(
+            self.base_url + "setSensorPosition",
+            json={
+                "device": self.device_id,
+                "coordinate": self.coordinate,
+                "linked_road_stretch": self.road_stretch
+            },
+            headers={"User-Agent": "curl/7.52.1"}
+        )
+        print("set_position", r)
+        print(r.text)
+
+    def send_data(self):
+        """Send randomly generated payload data to the backend"""
+
+        self.response = requests.post(
+            self.base_url + "put",
+            json=generate_payload(self.device_id),
+            headers={"User-Agent": "curl/7.52.1"}
+        )
+        print("send_data", self.response)
+
+    @classmethod
+    def get_all_roads(cls) -> dict:
+        """Get a list of all roads from the backend"""
+
+        response = requests.get(
+            cls.base_url + "getRoads",
+            headers={"User-Agent": "curl/7.52.1"}
+        )
+        content = json.loads(response.content)
+        if not content["success"]:
+            raise RuntimeError("Error fetching data from /getRoads")
+        return content["result"]
+
+    @classmethod
+    def get_roads(cls) -> dict:
+        """Get a list of all allowed roads from the backend"""
+
+        roads = cls.get_all_roads()
+        return {
+            k: roads[k] for k in roads
+            if roads[k]["road_type"] in cls.allowed_roads
+        }
+
+    @classmethod
+    def find_random_position(cls) -> tuple:
+        """Find a random position for a sensor"""
+
+        if cls.cached_roads is None or cls.cached_road_keys is None:
+            cls.cached_roads = cls.get_roads()
+            cls.cached_road_keys = list(cls.cached_roads.keys())
 
 
 if __name__ == "__main__":
