7f4c0b1337037239fae75ba8ba7fff204dd702ef
Chris <chris@hopfen.space>
2020-11-28 05:12:11 +0100

    Added a class to use multiple instances of the random traffic generator

--- api/gen_data.py
+++ api/gen_data.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 
+import sys
 import random
 import datetime
-import urllib.parse
-
-
-# Static data
-base_url = "https://lora.omikron.dev/api/"
-device_id = "random_data_generator"
 
 
 def generate_vehicle_data() -> dict:
     """Generate random vehicle data"""
 
-    return {str(k): random.randint(0, 5) for k in range(1, random.randint(0, 10))}
+    return {
+        str(k): random.randint(0, 5)
+        for k in range(1, random.randint(0, 10))
+    }
 
 
-def generate_payload() -> dict:
+def generate_payload(device_id: str) -> dict:
     """Generate random payload data for /put"""
 
     return {
@@ -34,4 +32,25 @@ def generate_payload() -> dict:
             ]
         }
     }
+
+
+class RandomTrafficGenerator:
+    """Generator for random traffic data"""
+
+    def __init__(self, device_id: str):
+        self.device_id = device_id
+        self.base_url = "https://lora.omikron.dev/api/"
+        self.allowed_roads = ["primary", "secondary", "tertiary"]
+
+
+if __name__ == "__main__":
+    count = 10
+    if len(sys.argv) > 1:
+        count = int(sys.argv[1])
+    [
+        RandomTrafficGenerator(
+            f"random_data_generator_{k}_{random.randint(1000, 100000)}"
+        )
+        for k in range(count)
+    ]
 
